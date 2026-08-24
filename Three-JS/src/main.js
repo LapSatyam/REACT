4aa1cf60fbc7c560785e08ea6fd96947c6bbfa4f
Satyam Sharma <lapsatyam@gmail.com>
2026-08-24 16:46:40 +0530

    Done

--- Three-JS/src/main.js
+++ Three-JS/src/main.js
@@ -1,57 +1,155 @@
-import './style.css'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import GUI from "lil-gui";
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const canvas = document.getElementById("canvas");
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
 
-<div class="ticks"></div>
+// Renderer
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.outputColorSpace = THREE.SRGBColorSpace;
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+// Loading materials
+const loader = new THREE.TextureLoader();
+const color = loader.load("./text/color.jpg");
+color.colorSpace = THREE.SRGBColorSpace;
+color.anisotropy = renderer.capabilities.getMaxAnisotropy();
+const rough = loader.load("./text/roughness.jpg");
+rough.anisotropy = renderer.capabilities.getMaxAnisotropy();
+const normal = loader.load("./text/normal.png");
+normal.anisotropy = renderer.capabilities.getMaxAnisotropy();
+const height = loader.load("./text/height.png");
+height.anisotropy = renderer.capabilities.getMaxAnisotropy();
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+// Creating a rectangle Geometry
+const geometry = new THREE.BoxGeometry(1.8, 0.9, 1, 32, 32, 32);
 
-setupCounter(document.querySelector('#counter'))
+// Applying materials
+const material = new THREE.MeshStandardMaterial({
+  map: color,
+  roughnessMap: rough,
+  normalMap: normal,
+  displacementMap: height,
+  displacementScale: 0.01,
+  metalness: 0.5,
+});
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+camera.position.z = 2.5;
+
+// Main / Key Light
+const keyLight = new THREE.DirectionalLight(0xffffff, 3);
+keyLight.position.set(5, 8, 6);
+keyLight.target.position.set(0, 0, 0);
+
+scene.add(keyLight);
+scene.add(keyLight.target);
+
+const fillLight = new THREE.DirectionalLight(0xffffff, 0.6);
+fillLight.position.set(-5, 3, 2);
+fillLight.target.position.set(0, 0, 0);
+
+scene.add(fillLight);
+scene.add(fillLight.target);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(ambientLight);
+
+// Resizer
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.025;
+controls.autoRotate = true;
+
+// GUI
+const gui = new GUI();
+
+const settings = {
+  color: "#00ff00",
+  scale: 1,
+};
+
+// Change color
+gui.addColor(settings, "color").onChange((value) => {
+  cube.material.color.set(value);
+});
+
+// rotation
+gui.add(controls, "autoRotate").name("Auto Rotate");
+gui.add(controls, "autoRotateSpeed", 0, 10, 0.1).name("Rotate Speed");
+
+// Change scale
+gui.add(settings, "scale", 0.1, 3).onChange((value) => {
+  cube.scale.set(value, value, value);
+});
+
+// Material
+const materialFolder = gui.addFolder("Material");
+
+materialFolder.add(material, "metalness", 0, 2, 0.01);
+materialFolder.add(material, "roughness", 0, 1, 0.01);
+materialFolder.add(material, "displacementScale", 0, 0.1, 0.001);
+
+// Lighting
+const lightFolder = gui.addFolder("Lighting").close();
+
+// ====================
+// Key Light
+// ====================
+
+const keyFolder = lightFolder.addFolder("Key Light");
+
+keyFolder.add(keyLight, "intensity", 0, 10, 0.1);
+
+keyFolder.addColor(keyLight, "color").name("Color");
+
+keyFolder.add(keyLight.position, "x", -20, 20, 0.1).name("Position X");
+keyFolder.add(keyLight.position, "y", -20, 20, 0.1).name("Position Y");
+keyFolder.add(keyLight.position, "z", -20, 20, 0.1).name("Position Z");
+
+// ====================
+// Fill Light
+// ====================
+
+const fillFolder = lightFolder.addFolder("Fill Light").close();
+
+fillFolder.add(fillLight, "intensity", 0, 5, 0.1);
+
+fillFolder.addColor(fillLight, "color").name("Color");
+
+fillFolder.add(fillLight.position, "x", -20, 20, 0.1).name("Position X");
+fillFolder.add(fillLight.position, "y", -20, 20, 0.1).name("Position Y");
+fillFolder.add(fillLight.position, "z", -20, 20, 0.1).name("Position Z");
+
+// ====================
+// Ambient Light
+// ====================
+
+const ambientFolder = lightFolder.addFolder("Ambient Light").close();
+
+ambientFolder.add(ambientLight, "intensity", 0, 2, 0.05);
+
+ambientFolder.addColor(ambientLight, "color").name("Color");
+
+// Animation
+function animate(time) {
+  renderer.render(scene, camera);
+  // cube.rotation.x = time / 2000;
+  // cube.rotation.y = time / 1000;
+  controls.update();
+}
+renderer.setAnimationLoop(animate);
